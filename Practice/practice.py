b7cd51e17579f708cded6d4a52834a3b4ef8a02a
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-04-08 08:09:42 +0600

    Practice string length, character access, and slicing techniques

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,1 +1,65 @@
-print('allahkuakaber')
+name = 'abdullah';
+age = 23;
+
+male = True;
+
+
+print('My Name is :', type (name), 'age:',type(age),'Gender:' , type(male))
+
+
+
+''' conditional statement'''
+
+light = input('Light :')
+
+if(light == 'red'):
+    print('stop')
+elif(light == 'yellow'):
+    print('wait')      
+elif(light == 'green'):
+    print('gooooo')  
+else:
+    print('you must wait  for traficc')   
+
+'''ternary operator '''
+light = input('Light: ')
+print('stop' if light == 'red' else 'wait' if light == 'yellow' else 'gooooo' if light == 'green' else 'you must wait for traffic')
+
+ 
+age = 18
+status = "Adult" if age >= 18 else "Minor"
+print(status)
+
+'''logical operation'''
+
+print(not False)
+print(not True)
+
+
+ball = True;
+ball2 = False
+
+print(ball and ball2); ''''2 tai ture hote hbe '''
+
+
+ball = True;
+ball2 = False
+
+print(ball or ball2); ''''1 tai ture hote hbe '''
+
+'''type conversion '''
+age = '23'
+nim = 29.89
+
+agess = int(age)
+prin = int(nim)
+
+print(type(prin))    # <class 'int'>
+print(type(age))     # <class 'str'>
+print(type(agess))   # <class 'int'>
+
+
+a = int(input("Enter a number: "))
+b = float(input("Enter a decimal: "))
+print(a + b)
+
---
+++ Practice/string.py
@@ -0,0 +1,37 @@
+str1= 'abdullahhhhh'
+str2 = 'software Engineer'
+
+print( len (str1 + '' + str2))
+
+ 
+
+abd = 'abdullah al kawser'
+print(len(abd))
+
+abd = 'abdullah al kawser'
+print(abd[12])
+print(abd.index('k'))  # Output: 0 (first 'a' is at index 0)
+
+'''Python Slicing Basic Syntax:'''
+
+name = "abdullah al kawser"
+print(name[0:8])     # Output: 'abdullah প্রথম ৮টা অক্ষর: (default: 1)'
+print(name[9:11])    # Output: 'al  শুধু 'al' বের করতে:'
+print(name[-6:])     # Output: 'kawser ৩. শেষে ৬ অক্ষর:'
+print(name[0:8:2])   # Output: 'adlah মাঝখান থেকে শুরু করে skip করে বের করা:' (0→2→4→6)
+print(name[::-1])    # Output: 'reswak la halludba  পুরো string reverse করে দেখা:'
+
+
+quote = "Python is powerful!"
+
+print(quote[9:15])  
+print(quote[14:19])  
+print(quote[::-1])  
+
+
+fruits = ["apple", "banana", "cherry", "mango", "pineapple"]
+
+print(fruits[0:3])   # ['apple', 'banana', 'cherry']
+print(fruits[-2:])   # ['mango', 'pineapple']
+print(fruits[::2])   # ['apple', 'cherry', 'pineapple']
+print(fruits[::-1])  # ['pineapple', 'mango', 'cherry', 'banana', 'apple']
