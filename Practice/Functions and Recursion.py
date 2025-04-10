bf0b37198f7b3aeb01a4c15b40f585d690928415
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-04-10 23:15:27 +0600

    function in python

---
+++ Practice/Functions and Recursion.py
@@ -0,0 +1,14 @@
+# def function_name(parameters):
+#     # code to be executed
+#     return result  # optional
+
+
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("abdullah al akawser")  # Calling the function
+
+
+def abdul (names,job):
+    print('Hellow ' + names , 'he is best ' + job)
+abdul('kstp','softw')    
