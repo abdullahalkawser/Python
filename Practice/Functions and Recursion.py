9d25241c5f732fe411c7aa70462e988af060f7d5
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-04-11 05:04:17 +0600

    Add multiple function examples in Python including recursion, default args, nested functions
    - Implemented simple greet functions with and without parameters
    - Demonstrated use of default parameters (`greet(name="Anisa")`)
    - Added examples of functions with multiple arguments
    - Included nested functions (inner function inside outer)
    - Showed recursion via `factorial` and custom countdown function
    - Added list handling and print functions for list length
    - Added a currency converter from USD to BDT
    - Demonstrated calling functions with various types of inputs

--- Practice/Functions and Recursion.py
+++ Practice/Functions and Recursion.py
@@ -11,4 +11,66 @@ greet("abdullah al akawser")  # Calling the function
 
 def abdul (names,job):
     print('Hellow ' + names , 'he is best ' + job)
-abdul('kstp','softw')    
+abdul('kawser','software Engineer')     
+abdul('kawserkstps','software Engineer and data scince')  
+abdul('kawserssssssssss','software Engineersssssssssssss')  
+
+
+def sumNumber (a,b,c):
+   sum =(a+b+c)
+   print(sum)
+   return sum
+sumNumber(20,3,4 )    
+
+''' Function with Default Parameter'''
+def greet(name="Anisa"): 
+    print("Hello,", name)
+
+greet()          # uses default
+greet("Rahman")  # overrides default
+
+
+''' Nested Function (Function inside another function)'''
+
+def outer():
+    def inner():
+        print("I am inner!")
+    print("I am outer!")
+    inner()
+
+outer()
+
+
+
+'''print list '''
+city = ['Rangpur', 'Dhaka', 'Chattogram']
+country = ['Bangladesh', 'Bangladesh', 'Bangladesh','Bangladesh', 'Bangladesh', 'Bangladesh']
+
+def printlenth(list):
+    print(len(list))
+
+printlenth(city)
+printlenth(country)
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(5))
+
+
+def contertaka (usdval):
+    takavalue= usdval *120
+    print(takavalue,usdval)
+
+contertaka(100)
+
+'''recursion'''
+def show (n):
+    if(n==0):
+        return
+    print(n)
+    show(n-1)
+show(10)    
